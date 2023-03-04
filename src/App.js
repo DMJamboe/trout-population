fc5c2acc8fbf173f29b1a8b0560283ed4e1f79d3
DMJamboe <theamazingnox@gmail.com>
2023-03-04 14:12:58 +0000

    Made basic web interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    if (input.length) {
+      alert(input);
+    }
+  }, [input]);
+  function getInput() {
+    setInput(document.getElementById("input-text").value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div class="App-header">
+        <h1>Trout Population Forecast</h1>
+      </div>
+      <div class="App-input">
+        <div class="input-inline">
+          <input type="text" id="input-text" placeholder="Write something here..."></input>
+          <input type="button" value="Forecast" onClick={getInput}></input>
+        </div>
+      </div>
+      <div class="App-water">
+        <div class="water-wave">
+          <span class="wave"></span>
+          <span class="deep-water"></span>
+        </div>
+      </div>
+    </div>    
   );
 }
 
